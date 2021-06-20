f788de27f6b1b9d763a670baedf77c2dd42a22b8
distorition <85959781+distorition@users.noreply.github.com>
2021-06-20 10:37:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -82,7 +82,7 @@ namespace Lesson_2_Geek_Brains
             }
             else
             {
-                Console.WriteLine(  "ff");
+                Console.WriteLine(  "Не ту такой поры ");
             }
 
 
